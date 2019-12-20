bb307728398684969fa685c254f773ca9c3f670a
Mevaser Rotner <ncar12345@gmail.com>
2019-12-20 10:39:28 +0200

    added draw.io

--- CPP_7/Source.cpp
+++ CPP_7/Source.cpp
@@ -46,8 +46,9 @@ void main()
 	// msgToGraphics should contain the board string accord the protocol
 	// YOUR CODE
 
-	strcpy_s(msgToGraphics, "rnbkqbnrpppppppp################################PPPPPPPPRNBKQBNR1"); // just example...
-	
+	//strcpy_s(msgToGraphics, "rnbkqbnrpppppppp################################PPPPPPPPRNBKQBNR1"); // just example...
+	strcpy_s(msgToGraphics, "r######r################################################R######R#"); // just example...
+
 	p.sendMessageToGraphics(msgToGraphics);   // send the board string
 
 	// get message from graphics
